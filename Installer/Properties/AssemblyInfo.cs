71b6a9b675da87df03856b8cdce4fe66e84a8411
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-07-12 18:21:15 +0200

    Set .NET 8.0.18 as Requirement

--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Fenix2GSX Installer v0.51.0.0 (2025.07.12.1550)")]
+[assembly: AssemblyTitle("Fenix2GSX Installer v0.51.0.0 (2025.07.12.1620)")]
 [assembly: AssemblyDescription("Installer Application for Fenix2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]
